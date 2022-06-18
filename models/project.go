9f9005d61131951ab5fa8ee363b67050e5dea67d
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2022-06-18 12:58:12 +0300

    Update projects API

--- models/project.go
+++ models/project.go
@@ -3,10 +3,11 @@ package models
 type Projects []Project
 
 type Project struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Image       string `json:"image"`
-	Links       []struct {
+	Title            string   `json:"title"`
+	Description      string   `json:"description"`
+	DescriptionShort string   `json:"descriptionShort"`
+	Images           []string `json:"images"`
+	Links            []struct {
 		Url  string `json:"url"`
 		Type string `json:"type"`
 	} `json:"links"`
